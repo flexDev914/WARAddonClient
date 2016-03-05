c5e6303ed9fd7d96a9cbc72df8048f38a92c7e06
flexDev914 <flexdev17@gmail.com>
2016-03-05 23:59:41 +0100

    checking local version adding search adding sorting

--- src/Data/Addon.java
+++ src/Data/Addon.java
@@ -27,6 +27,54 @@ public class Addon {
             tags.add(tagList.getString(counter));
             counter++;
         }
+        findInstalled();
+    }
+    protected java.io.File findMatch(java.io.File folder,String search) {
+            if(!folder.exists()) {
+                return null;
+            }
+            for(java.io.File innerFolder : folder.listFiles()) {
+                if(innerFolder.getName().equalsIgnoreCase(search)) {
+                    return innerFolder;
+                }
+            }
+            return null;
+    }
+    protected java.io.File getAddonFolder() {
+        String[] path= new String[3];
+        path[0]="interface";
+        path[1]="addons";
+        path[2]=name;
+        java.io.File folder = new java.io.File("./");
+        for(int counter=0;counter<3;counter++) {
+            folder=findMatch(folder,path[counter]);
+            if(folder==null||!folder.exists()) {
+                return null;
+            }
+        }
+        return folder;
+    }
+    protected final void findInstalled() {
+        java.io.File folder=getAddonFolder();
+        if(folder==null||!folder.exists()) {
+            return;
+        }
+        for (java.io.File fileEntry : folder.listFiles()) {
+            if (
+                !fileEntry.isDirectory() &&
+                org.apache.commons.io.FilenameUtils.getExtension(fileEntry.getName()).equalsIgnoreCase("mod")
+            ){
+                try{
+                    org.w3c.dom.NodeList list=javax.xml.parsers.DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(fileEntry).getElementsByTagName("UiMod");
+                    for(int counter=0;counter<list.getLength();counter++) {
+                        this.installed=list.item(counter).getAttributes().getNamedItem("version").getTextContent();
+                    }
+                }
+                catch(javax.xml.parsers.ParserConfigurationException | javax.xml.parsers.FactoryConfigurationError | org.xml.sax.SAXException | java.io.IOException exception){
+                    System.out.println(exception.getMessage());
+                }
+            }
+        }
     }
     protected final String getStringFromObject(String key,javax.json.JsonObject data) {
         String value="";
@@ -44,5 +92,8 @@ public class Addon {
     }
     public String getDescription() {
         return "<html>"+description;
+    }
+    public String getName() {
+        return name;
     }
 }
--- src/Gui/Wrapper.java
+++ src/Gui/Wrapper.java
@@ -11,6 +11,7 @@ package Gui;
  */
 public class Wrapper extends javax.swing.JFrame {
     Data.AddonList addons=new Data.AddonList();
+    javax.swing.table.TableRowSorter sorter;
     /**
      * Creates new form Wrapper
      */
@@ -26,6 +27,18 @@ public class Wrapper extends javax.swing.JFrame {
         InstallButton.setEnabled(false);
         RemoveButton.setEnabled(false);
         this.setIconImage(java.awt.Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Images/logo.png")));
+        sorter = new javax.swing.table.TableRowSorter(this.AddonList.getModel());
+        this.AddonList.setRowSorter(sorter);
+    }
+    private void newFilter() {
+        javax.swing.RowFilter rf = null;
+        //If current expression doesn't parse, don't update.
+        try {
+            rf = javax.swing.RowFilter.regexFilter(Search.getText(), 0);
+        } catch (java.util.regex.PatternSyntaxException e) {
+            return;
+        }
+        sorter.setRowFilter(rf);
     }
     protected final void makeAddonList() {
         javax.json.JsonArray parse = request.getAddonList();
@@ -45,7 +58,7 @@ public class Wrapper extends javax.swing.JFrame {
         if(!new java.io.File("./RoRLauncher.exe").exists()) {
             errors+="Missing RoRLauncher.exe here, please put this file in the Warhammer Online directory.";
         }
-        if(false&&errors.length()>0) {
+        if(errors.length()>0) {
             javax.swing.JOptionPane.showMessageDialog(this,errors);
             this.dispose();
             System.exit(0);
@@ -54,6 +67,7 @@ public class Wrapper extends javax.swing.JFrame {
     class tableListener implements javax.swing.event.ListSelectionListener {
         public void valueChanged(javax.swing.event.ListSelectionEvent event) {
             Description.setText(addons.get(AddonList.getSelectedRow()).getDescription());
+            Title.setText(addons.get(AddonList.getSelectedRow()).getName());
             InstallButton.setEnabled(true);
             RemoveButton.setEnabled(true);
         }
@@ -78,6 +92,7 @@ public class Wrapper extends javax.swing.JFrame {
         Description = new javax.swing.JLabel();
         InstallButton = new javax.swing.JButton();
         RemoveButton = new javax.swing.JButton();
+        Title = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,7 +106,13 @@ public class Wrapper extends javax.swing.JFrame {
                 SearchActionPerformed(evt);
             }
         });
+        Search.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                SearchKeyReleased(evt);
+            }
+        });
 
+        AddonList.setAutoCreateRowSorter(true);
         AddonList.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
@@ -115,6 +136,7 @@ public class Wrapper extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
+        AddonList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         AddonList.getTableHeader().setReorderingAllowed(false);
         jScrollPane2.setViewportView(AddonList);
         AddonList.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
@@ -161,6 +183,8 @@ public class Wrapper extends javax.swing.JFrame {
 
         RemoveButton.setText("Remove");
 
+        Title.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -168,7 +192,9 @@ public class Wrapper extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addComponent(InstallButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(Title, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(RemoveButton)
                 .addGap(24, 24, 24))
             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -180,9 +206,10 @@ public class Wrapper extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(RemoveButton)
-                    .addComponent(InstallButton))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 414, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(InstallButton)
+                    .addComponent(Title))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -209,8 +236,12 @@ public class Wrapper extends javax.swing.JFrame {
     }//GEN-LAST:event_InstallButtonActionPerformed
 
     private void SearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SearchActionPerformed
-        // TODO add your handling code here:
+        newFilter();
     }//GEN-LAST:event_SearchActionPerformed
+
+    private void SearchKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_SearchKeyReleased
+        newFilter();
+    }//GEN-LAST:event_SearchKeyReleased
 
     /**
      * @param args the command line arguments
@@ -253,6 +284,7 @@ public class Wrapper extends javax.swing.JFrame {
     private javax.swing.JButton InstallButton;
     private javax.swing.JButton RemoveButton;
     private javax.swing.JTextField Search;
+    private javax.swing.JLabel Title;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
