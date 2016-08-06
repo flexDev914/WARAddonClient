c87813d60b9e17141fc51e62091d759398bcd82d
flexDev914 <flexdev17@gmail.com>
2016-08-06 16:07:34 +0200

    reformatting

--- src/main/java/de/idrinth/waraddonclient/implementation/model/NoAddon.java
+++ src/main/java/de/idrinth/waraddonclient/implementation/model/NoAddon.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.idrinth.waraddonclient.implementation.model;
 
 public class NoAddon implements de.idrinth.waraddonclient.interfaces.model.Addon {
