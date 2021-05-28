cef9e0fa2c6e7d63247bb1cc40d3dd96e6573a5f
flexDev914 <flexdev17@gmail.com>
2021-05-28 12:43:28 +0200

    added command options for restore and backup

--- src/main/java/de/idrinth/waraddonclient/CliMain.java
+++ src/main/java/de/idrinth/waraddonclient/CliMain.java
@@ -2,19 +2,22 @@ package de.idrinth.waraddonclient;
 
 import de.idrinth.waraddonclient.cli.AliasOption;
 import de.idrinth.waraddonclient.cli.AliasParser;
-import de.idrinth.waraddonclient.service.Config;
+import de.idrinth.waraddonclient.service.*;
 import de.idrinth.waraddonclient.model.CmdAddonList;
 import de.idrinth.waraddonclient.service.logger.CliLogger;
-import de.idrinth.waraddonclient.service.FileSystem;
 import de.idrinth.waraddonclient.service.logger.MultiLogger;
-import de.idrinth.waraddonclient.service.Request;
-import de.idrinth.waraddonclient.service.XmlParser;
+
 import javax.xml.parsers.ParserConfigurationException;
+
+import net.lingala.zip4j.exception.ZipException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+
+import java.io.File;
+import java.io.IOException;
 
 public class CliMain extends BaseMain {
 
@@ -34,6 +37,8 @@ public class CliMain extends BaseMain {
         options.addOption("i", "install", true, "Install/Update given addon.");
         options.addOption("r", "remove", true, "Remove given addon.");
         options.addOption("h", "help", false, "This output.");
+        options.addOption("b", "backup", false, "Backups all addons.");
+        options.addOption("z", "restore", true, "Restores from a specified backup.");
 
         CommandLineParser parser = new AliasParser();
         try {
@@ -70,7 +75,20 @@ public class CliMain extends BaseMain {
                 logger.info("Removed " + cli.getOptionValue("remove"));
                 return;
             }
-        } catch(ParseException ex) {
+            if (cli.hasOption("backup")) {
+                Backup bk = new Backup(config);
+                bk.create();
+                logger.info("Created a backup");
+                return;
+            }
+            if (cli.hasOption("restore")) {
+                Backup bk = new Backup(config);
+                File restore = new File(cli.getOptionValue("restore"));
+                bk.restore(restore);
+                logger.info("Restored from file " + restore.getAbsolutePath());
+                return;
+            }
+        } catch(ParseException | IOException ex) {
             logger.error(ex);
         }
         HelpFormatter formatter = new HelpFormatter();
